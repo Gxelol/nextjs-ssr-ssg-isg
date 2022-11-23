8aba2d6fe623acf13f0bff202db10f07919b9563
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-22 22:34:21 -0300

    eslint, prettier, editorconfig, prop-types

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import styled from 'styled-components';
 
 const Heading = styled.h1`
-  background: ${({ theme }) => theme.colors.secondaryColor };
+  background: ${({ theme }) => theme.colors.secondaryColor};
 `;
 
 export default function Home() {
@@ -10,5 +10,5 @@ export default function Home() {
     <div>
       <Heading>Hello World</Heading>
     </div>
-  )
+  );
 }
