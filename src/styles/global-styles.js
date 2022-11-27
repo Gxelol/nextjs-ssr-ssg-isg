dde90c864aa880854e7e9a1b5e2e5ea50a5e6632
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-26 23:39:58 -0300

    font style

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
@@ -16,12 +14,12 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     font-size: 1.6rem;
-    font-family: 'Open-sans', sans-serif;
+    font-family: ${({ theme }) => theme.fonts.family.default};
     min-width: 100%;
   }
 
   h1, h2, h3, h4, h5, h6 {
-    font-family: 'Montserrat', sans-serif;
+    font-family: ${({ theme }) => theme.fonts.family.secondary};
     margin: ${({ theme }) => theme.spacings.large} 0;
     word-wrap: break-word;
   }
