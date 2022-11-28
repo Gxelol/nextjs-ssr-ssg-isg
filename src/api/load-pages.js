22fd70d496a48d2b0682253bf47842ab478f3900
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-11-27 23:38:14 -0300

    refatoration + notFound

---
+++ src/api/load-pages.js
@@ -0,0 +1,16 @@
+import config from '../config';
+import { mapData } from './map-data';
+
+export const loadPages = async (slug = '') => {
+  const cleanSlug = slug ? `slug=${slug.replace(/[^a-z0-9-_]/gi, '')}` : '';
+  const url = `${config.url}/pages/${cleanSlug}`;
+
+  // const raw = await fetch(url); // Correct
+  const raw = await fetch(config.url); // With this because it's without servers
+  const json = await raw.json();
+  const { attributes } = json.data[0];
+  const pageData = mapData([attributes]);
+  const data = pageData;
+
+  return data;
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,25 @@
-import P from 'prop-types';
-// import Head from 'next/head';
-// import styled from 'styled-components';
-
-import config from '../config';
-import { mapData } from '../api/map-data';
 import Home from '../templates/Home';
+import P from 'prop-types';
+import { loadPages } from '../api/load-pages';
 
-export default function Index({ data = null }) {
+export default function Index({ data }) {
   return <Home data={data} />;
 }
 
 export const getStaticProps = async () => {
-  const raw = await fetch(config.url);
-  const json = await raw.json();
-  const { attributes } = json.data[0];
-  const pageData = mapData([attributes]);
-  const data = pageData;
+  let data;
+
+  try {
+    data = await loadPages('landing-page');
+  } catch {
+    data = null;
+  }
+
+  if (!data || !data.length) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
